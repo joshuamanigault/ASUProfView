3dad1f701235583c5d5a2ad39f25d92c31d9563a
Joshua's Laptop <norcaz919@gmail.com>
2025-09-18 16:20:07 -0700

    Testing Vite for bundling; API not utilized YET

---
+++ my-extension/vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "vite";
+import { copyFileSync } from "fs";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        content: "src/content.js",
+        background: "src/background.js",
+      },
+      output: {
+        entryFileNames: "[name].js", // content.js, background.js
+      },
+    },
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+  plugins: [
+    {
+      name: "copy-manifest",
+      writeBundle() {
+        copyFileSync(
+          resolve("src/manifest.json"),
+          resolve("dist/manifest.json")
+        );
+      },
+    },
+  ],
+});
