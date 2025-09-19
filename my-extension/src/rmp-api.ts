7f939647ea97e254ec0d76ea1ee76c6c3e59334f
Joshua's Laptop <norcaz919@gmail.com>
2025-09-18 20:40:23 -0700

    API works alone; Logic needs to be moved into the extension

--- my-extension/src/rmp-api.ts
+++ my-extension/src/rmp-api.ts
@@ -1,20 +1,8 @@
 import { RateMyProfessor } from "rate-my-professor-api-ts";
+// https://www.ratemyprofessors.com/professor/legacyId
 
-// accepts only 1 parameter
-// which is the name of the college of interest
 (async function main() {
-    const rmp_instance = new RateMyProfessor("City College of New York");
- 
-    // one asynchronous method helps retrieve information reagrding college
- 
-    // method takes in a boolean
-    // if boolean is set to true, similar matching named college info will be returned
-    // if boolean is set to false, only, the specific college will be returned
-    //
-    let college_info = await rmp_instance.get_college_info(false);
-    let college_info_all = await rmp_instance.get_college_info(true);
- 
-   
-    console.log(college_info);
-    console.log(college_info_all);
- })();
+    const rmp_instance = new RateMyProfessor("Arizona State University", "Darryl Reano");
+
+    console.log(await rmp_instance.get_professor_info());
+})();
